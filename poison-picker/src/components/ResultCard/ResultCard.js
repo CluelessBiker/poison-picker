643795fbfb5d81cce6228347ee886cd5fe4fb237
Lauren-Nicole <popich.red@gmail.com>
2022-10-13 11:31:29 +0000

    more changes.

--- poison-picker/src/components/ResultCard/ResultCard.js
+++ poison-picker/src/components/ResultCard/ResultCard.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./ResultCard.module.css";
 
-const ResultCard = () => {
+const ResultCard = ({ card }) => {
   return (
-    <div>ResultCard</div>
-  )
-}
+    <div className={styles.main}>
+        <div>
+        <h2 className={styles.drinkName}>{card.strDrink}</h2>
+      </div>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={card.strDrinkThumb} alt={card.strDrink}/>
+      </div>
+      <div>
+        <Link className={styles.drinkLink} to={`/recipe/${card.idDrink}`}>See Recipe</Link>
+      </div>
+    </div>
+  );
+};
 
-export default ResultCard
+export default ResultCard;
